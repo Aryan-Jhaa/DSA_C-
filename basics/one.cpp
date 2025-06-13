b2109651b6d3be72b1844d6349458fcfa3422acf
aryan813 <aryanjha813@gmail.com>
2025-06-13 14:05:51 +0530

    Update file one

--- basics/one.cpp
+++ basics/one.cpp
@@ -1,11 +1,30 @@
 // Starting DSA w c++
 
 #include <iostream>
-
+#include<string>
 using namespace std;
 
 int main(){
     cout << "Learning DSA for placements" << endl;
+
+// If-Else:
+
+    string userInput;
+    cout << "What is your favourite type of tea: \n";
+
+    getline(cin, userInput);
+
+    if (userInput == "Ice Tea")
+     {
+        cout << "User's favourite is Ice Tea" << endl;
+     }
+    else
+     {
+        cout << "User's favourite is " << userInput << endl;
+     }
+    
+    
     return 0;
+
 }
 
