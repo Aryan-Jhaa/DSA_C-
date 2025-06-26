b43ec15ea6d58e43a231162a4bdb7a5b57644e3e
aryan813 <aryanjha813@gmail.com>
2025-06-26 20:56:30 +0530

    Start neetcode

--- basics/five.cpp
+++ basics/five.cpp
@@ -4,13 +4,12 @@ using namespace std;
 
 void printPattern(int n){
 
- for(int i=1; i<=n; i++){
-
-        for (int j = 0; j <n-i+1; j++)
-        {
-            cout << "* ";
+ for(int i=0; i<n; i++){
+        //Space
+        for (int j = 1; j <=n-i+1; j++){
+            cout << j << " ";
         }
-        cout << endl;        
+        c       
     }    
 }
 
